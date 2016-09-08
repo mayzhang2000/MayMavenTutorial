8b43d3acabbe57ce72fbb38a64ea967b2b7fb406
mayz985 <may_zhang@intuit.com>
2016-09-08 14:25:53 -0700

    Add PatternBuilder

---
+++ car/src/main/java/PatternBuilder.java
@@ -0,0 +1,34 @@
+/**
+ * Created by mayz985 on 9/8/16.
+ */
+
+
+
+public class PatternBuilder {
+    String name;
+
+    private PatternBuilder(String name) {
+        this.name = name;
+    }
+
+    public static class CarWithBuilderDemoBuilder {
+        String name;
+        public CarWithBuilderDemoBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+        public PatternBuilder build() {
+            return new PatternBuilder(this.name);
+        }
+
+    }
+
+    public void printName() {
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        new PatternBuilder.CarWithBuilderDemoBuilder().withName("lexus").build().printName();
+    }
+
+}
